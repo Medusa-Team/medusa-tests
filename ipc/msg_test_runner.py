610e1d1d692e4d0438e3e966b66d8f2283f99a91
Matus Jokay <matus.jokay@gmail.com>
2022-01-31 20:12:41 +0100

    feat: add python runner script for msg_test

---
+++ ipc/msg_test_runner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+
+subprocess_count = 300
+
+def main():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+
+    for i in range(subprocess_count):
+        proc = subprocess.Popen('./msg_test', cwd=cwd)
+        print(proc.pid)
+
+if __name__ == '__main__':
+    main()
