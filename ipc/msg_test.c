b1eb3734bc97831b2ff593057e7dd71d4ac3eb11
Matus Jokay <matus.jokay@stuba.sk>
2022-02-18 11:02:44 +0100

    feat: add command line arguments to message queue test

--- ipc/msg_test.c
+++ ipc/msg_test.c
@@ -5,19 +5,25 @@
 #include <time.h>
 #include <signal.h>
 #include <wait.h>
+#include <getopt.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/wait.h>
 #include <sys/errno.h>
 #include <sys/types.h>
 
-extern int errno;       // error NO.
 #define MSGTXTLEN 1024  // msg text length
 #define MSGKEY	0x1234	// can be also IPC_PRIVATE
-#define TASKCNT 10000	// count of tasks
-#define WORKINGTIME 30	// time in secs to work
+#define WORKERS 1000	// count of msg senders and receivers together 
+#define WORKERS_MAX 10000
+#define WORKERS_MIN 1
+#define TIMEOUT 10	// runtime of the test in secs
+#define TIMEOUT_MAX 300
+#define TIMEOUT_MIN 2
 
 #define MSGGETTYPE()	((rand() % 5) + 1)
+
+extern int errno;
 
 struct msg {
   long mtype;
@@ -129,12 +135,79 @@ void rcv_msg(int msq, int print)
   }
 }
 
-int main()
+void help(char *progname)
 {
-  int msq, i, not_finished, choice;
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Usage: %s [--help | -h] [--workers=N] [--timeout=T]\n\n", progname);
+  fprintf(stderr, "Test message queue IPC subsystem.\n\n");
+  fprintf(stderr, "Program tries to create N workers (through fork() system call);\n");
+  fprintf(stderr, "each worker (randomly choosen) is one of msg sender or receiver.\n");
+  fprintf(stderr, "\n");
+  fprintf(stderr, "Options\n");
+  fprintf(stderr, "  --help\tThis message ;)\n");
+  fprintf(stderr, "  --workers=N\tThe count of workers. The default number of wor-\n");
+  fprintf(stderr, "\t\tkers is %d.\n", WORKERS);
+  fprintf(stderr, "\t\tN should be an integer in <%d; %d>.\n", WORKERS_MIN, WORKERS_MAX);
+  fprintf(stderr, "  --timeout=T\tThe duration of the test. The default timeout is\n");
+  fprintf(stderr, "\t\tset to %d secs. The timeout starts *after* crea-\n", TIMEOUT);
+  fprintf(stderr, "\t\ttion of all workers. So the duration of the test\n");
+  fprintf(stderr, "\t\tconsists from two parts: the time of workers cre-\n");
+  fprintf(stderr, "\t\tation and the run of them.\n");
+  fprintf(stderr, "\t\tT should be an integer in <%d; %d>.\n", TIMEOUT_MIN, TIMEOUT_MAX);
+  fprintf(stderr, "\n");
+  fprintf(stderr, "\n");
+}
+
+int main(int argc, char *argv[])
+{
+  int workers = WORKERS, timeout = TIMEOUT;
+  int msq, i, c, worker_type;
   int op[2]; // op[0] -- operation snd, op[1] -- operation rcv
-  pid_t pids[TASKCNT], pid;
+  pid_t pids[WORKERS_MAX], pid;
+  char buf[1024];
   void (*operation)(int, int);
+  static struct option long_options[] = {
+    {"help", no_argument, 0, 'h'},
+    {"workers", required_argument, 0, 'w'},
+    {"timeout", required_argument, 0, 't'},
+    {0, 0, 0, 0},
+  };
+
+  while ((c = getopt_long(argc, argv, ":h", long_options, 0)) != -1) {
+    switch (c) {
+    case 'h':
+      help(argv[0]);
+      return 0;
+    case ':':
+      fprintf(stderr, "Error: missing an argument of an option!\n");
+      help(argv[0]);
+      return -1;
+    case '?':
+      fprintf(stderr, "Error: invalid option!\n");
+      help(argv[0]);
+      return -1;
+    default:
+      help(argv[0]);
+      return -1;
+
+    case 'w':
+      memset(buf, '\0', sizeof(buf));
+      if (sscanf(optarg, "%d%s", &workers, buf) != 1 || strlen(buf) || workers < WORKERS_MIN || workers > WORKERS_MAX) {
+        fprintf(stderr, "Error: invalid argument of the option '--workers'.\n");
+        help(argv[0]);
+        return -1;
+      }
+      break;
+    case 't':
+      memset(buf, '\0', sizeof(buf));
+      if (sscanf(optarg, "%d%s", &timeout, buf) != 1 || strlen(buf) || timeout < TIMEOUT_MIN || timeout > TIMEOUT_MAX) {
+        fprintf(stderr, "Error: invalid argument of the option '--timeout'.\n");
+        help(argv[0]);
+        return -1;
+      }
+      break;
+    }
+  }
 
   // initialise rnd generator, variables and message queue
   srand(time(NULL));
@@ -142,58 +215,57 @@ int main()
   if ((msq = open_msq(MSGKEY, 0)) < 0)
     return -1;
 
-  // create TASKCNT tasks
-  fprintf(stderr, "wait a moment, generating message senders and receivers... ");
+  // create WORKERS tasks
+  fprintf(stderr, "wait a moment... I'm trying to generate %d message senders and receivers... ", workers);
   op[0] = op[1] = 0;
-  not_finished = TASKCNT;
-  for (i = 0; i < TASKCNT; i++) {
-    choice = rand() % 2;
-    op[choice]++;
+  for (i = 0; i < workers; i++) {
+    worker_type = rand() % 2;
+    op[worker_type]++;
 
     pids[i] = fork();
 
     // child
     if (pids[i] == 0) {
       // random choice rcv_msg/snd_msg
-      operation = choice ? rcv_msg : snd_msg;
+      operation = worker_type ? rcv_msg : snd_msg;
       operation(msq, 0);
       return 0;
     }
    
     // if we cannot produce another child, continue with those created yet
     if (pids[i] == -1) {
-      not_finished = i;
+      workers = i;
       pids[i] = 0;
-      i--;
-      op[choice]--;
+      op[worker_type]--;
       break;
     }
   }
-  fprintf(stderr, "done\ncontinue testing (%d sec) with %d childs alive: %d senders, %d receivers\n", WORKINGTIME, i+1, op[0], op[1]);
+  fprintf(stderr, "done\ntest continues for another %d secs with %d childs alive: %d senders, %d receivers\n",
+          timeout, workers, op[0], op[1]);
 
   // let workers to do something interesting...
-  sleep(WORKINGTIME);
+  sleep(timeout);
   // destroy message queue to finish workers
   close_msq(msq, 0);
   sleep(1);
 
   // clean up
   while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
-    for (i = 0; i < TASKCNT; i++) {
+    for (i = 0; i < WORKERS; i++) {
       if (pids[i] == pid) {
         pids[i] = 0;
-	not_finished--;
+	workers--;
 	break;
       }
     }
-    if (i == TASKCNT)
+    if (i == WORKERS)
       fprintf(stderr, "error: pid %d is not mY child!\n", pid);
   }
-  if (not_finished)
-   fprintf(stderr, "not finished %d from %d childs yet, I'll kill them ;)\n", not_finished, TASKCNT);
+  if (workers)
+   fprintf(stderr, "not finished %d from %d childs yet, I'll kill them ;)\n", workers, WORKERS);
 
   // kill not finished workers yet
-  for (i = 0; i < TASKCNT; i++) {
+  for (i = 0; i < WORKERS; i++) {
     if (pids[i] > 0) {
       kill(pids[i], SIGKILL);
       if (waitpid(pids[i], NULL, 0) != pids[i])
